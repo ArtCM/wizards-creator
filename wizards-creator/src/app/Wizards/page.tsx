6b1ddc39dc517ff7608e140885f86b4a426ebd13
ArtCM <arthurperfilprof@hotmail.com>
2024-02-08 16:49:25 -0300

    fix: Correção de classe duplicada no tailwind da pagina WizardsView

--- wizards-creator/src/app/Wizards/page.tsx
+++ wizards-creator/src/app/Wizards/page.tsx
@@ -11,7 +11,7 @@ export default function WizardsView() {
     return (
         <>
             <h1 className="font-bold text-3xl mb-5 border-b-2 border-gray-300">Wizards Criadas</h1>
-            <div className="flex items-center justify-center w-full grid lg:grid-cols-4 grid-cols-1">
+            <div className="items-center justify-center w-full grid lg:grid-cols-4 grid-cols-1">
 
                 <WizardCard 
                     title="Titulo 1"
