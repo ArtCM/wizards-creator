872ab4d9d065a4073616296db275b1f4de46ab83
ArtCM <arthurperfilprof@hotmail.com>
2024-02-09 06:56:41 -0300

    feat: adicionado lazy loading na pagina de criação das wizards para garantir o carregamento completo antes da utilização

--- wizards-creator/src/app/Creator/page.tsx
+++ wizards-creator/src/app/Creator/page.tsx
@@ -1,12 +1,31 @@
+'use client'
 import { WizardCreate } from "../components/createWizard";
+import React, { useState, useEffect } from 'react';
+
+
+
 
 export default function Creator() {
+    const [loaded, setLoaded] = useState(false);
+  
+    useEffect(() => {
+      // Simula um carregamento assíncrono
+      setTimeout(() => {
+        setLoaded(true);
+      }, 2000);
+    }, []);
+
     return (
         <>
             <h1 className="font-bold text-3xl mb-5 border-b-2 border-gray-300">Criador de Wizards</h1>
             <div className="flex items-center justify-center flex-col w-full">
-                <WizardCreate />
+                {loaded ? (
+                    <WizardCreate />
+                ) : (
+                    <div>Carregando...</div>
+                )}
             </div>
+            
         </>
     );
 }
